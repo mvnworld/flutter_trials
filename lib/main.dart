86eba2384aa425ee792c1eddedd0c4e067086757
Mangesh <mangesh.nadkarni@gmail.com>
2020-08-18 16:16:53 -0400

    Changed main dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Sudeep Demo Home Page'),
+      home: MyHomePage(title: 'Test Demo Home Page'),
     );
   }
 }
